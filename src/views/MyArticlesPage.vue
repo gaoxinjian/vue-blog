b1d3023ffd2adad8af4a2fcc727dd27d94bf2126
杲新建 <ggalaxy4123@gmail.com>
2026-01-02 13:15:10 +0800

    修复build报错

---
+++ src/views/MyArticlesPage.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="my-articles-page">
+    <h1>My Articles</h1>
+  </div>
+</template>
---
+++ src/views/WriteArticlePage.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="write-article-page">
+    <h1>Write Article Page</h1>
+  </div>
+</template>
